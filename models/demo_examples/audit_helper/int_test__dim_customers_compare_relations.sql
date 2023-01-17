c6d76f3f998a04a4b4f788ed692abab71c6f2fa6
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-01-17 16:16:10 +0000

    make integration test conditional on non-prod environment runs

--- models/demo_examples/audit_helper/int_test__dim_customers_compare_relations.sql
+++ models/demo_examples/audit_helper/int_test__dim_customers_compare_relations.sql
@@ -1,17 +1,21 @@
 {# use this if you want to map the audit testing data into the DAG #}
 {# set prod_relation = source('production_data_clones', 'dim_customers') -#}
 
-{% set prod_relation = api.Relation.create(
+{% if target.name != 'prod' %}
+  
+  {% set prod_relation = api.Relation.create(
       database=target.database,
       schema='prod_data_clones',
       identifier='dim_customers',
       type='table'
-) -%}
+  ) -%}
 
-{% set candidate_relation = ref('dim_customers') %}
+  {% set candidate_relation = ref('dim_customers') %}
 
-{{ audit_helper.compare_relations(
+  {{ audit_helper.compare_relations(
     a_relation=prod_relation,
     b_relation=candidate_relation,
     primary_key="customer_key"
-) }}
+  ) }}
+
+{% endif %}
