4c99d785b125665ce8b494f86a87a42c24d14e8d
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-04-24 17:02:56 -0600

    reorganize repo

---
+++ dbt_transform_code/models/staging/tpch/stg_tpch_regions.sql
@@ -0,0 +1,19 @@
+with source as (
+
+    select * from {{ source('tpch', 'region') }}
+
+),
+
+rename as (
+
+    select
+
+        r_regionkey as region_key,
+        r_name as name,
+        r_comment as comment
+
+    from source
+
+)
+
+select * from rename
