961fb27c7f28b9c610a4138f8eb360ea8e9a67eb
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-04-25 18:48:29 +0000

    simpler custom schema config

--- macros/config/generate_schema_name.sql
+++ macros/config/generate_schema_name.sql
@@ -1,36 +1,15 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     
     {%- set default_schema = target.schema -%}
-    {%- set schema_prefix = target.name if target.name == 'ci' else '' -%}
     
-    {# behavior for models #}
-    {%- if node.resource_type == 'model' -%}
+    {%- if custom_schema_name is none -%}
 
-      {{ log('in model code path', info=true) }}
+    {{ default_schema }}
 
-      {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-      {%- else -%}
-
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-       
-      {%- endif -%}
-     
-    {# behavior for snapshots #}
     {%- else -%}
-       
-       {%- if custom_schema_name is none -%}
 
-        {{ default_schema }}
-
-      {%- else -%}
-
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-       
-      {%- endif -%}
-
+    {{ default_schema }}_{{ custom_schema_name | trim }}
+    
     {%- endif -%}
-
+     
 {%- endmacro %}
