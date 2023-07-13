7ae1b7a99a06e00052a6bad15b8aaaf25eaa1da5
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-07-13 21:09:51 +0000

    update fct_orders_incremental

--- models/marts/core/fct_orders_incremental.sql
+++ models/marts/core/fct_orders_incremental.sql
@@ -1,13 +1,13 @@
 {{
     config(
         materialized='incremental',
+        incremental_strategy='merge',
         unique_key='order_key'
     )
 }}
 
-select * from {{ref('fct_orders')}}
+select * 
+from {{ref('fct_orders')}}
 {% if is_incremental() %}
-where order_date >= '1998-07-01'
-{% else %}
-where order_date < '1998-07-01'
+where order_key not in (select order_key from {{ this }})
 {% endif %}
