9340c5a606cc7b301be6afa26a88e0fa478a6fb3
Matt Winkler <matt.winkler@fishtownanalytics.com>
2023-11-07 15:47:06 +0000

    remove on_schema_change which is broken.

--- models/marts/core/fct_orders_incremental.sql
+++ models/marts/core/fct_orders_incremental.sql
@@ -2,13 +2,14 @@
     config(
         materialized='incremental',
         unique_key='order_key',
-        on_schema_change='sync_all_columns'
+        incremental_strategy='merge',
     )
 }}
 
-select * from {{ref('fct_orders')}}
+select * 
+from {{ref('fct_orders')}}
 {% if is_incremental() %}
-where order_date >= '1998-07-01'
+where order_key not in (select order_key from {{this}} group by 1)
 {% else %}
 where order_date < '1998-07-01'
 {% endif %}
